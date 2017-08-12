13689a1730596787a62d778c58f07e5b678cb7e7
tako <rina.m.takase@gmail.com>
2017-08-12 20:20:28 +0900

    Annotate

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: stocks
+#
+#  id         :integer          not null, primary key
+#  product_id :string
+#  price      :integer
+#  rest       :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Stock < ApplicationRecord
 end
