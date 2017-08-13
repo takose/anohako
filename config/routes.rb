d040bae41a151b55f7586ff30057e5749fe4790f
tako <rina.m.takase@gmail.com>
2017-08-13 16:55:27 +0900

    Routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,9 @@
 # 
 
 Rails.application.routes.draw do
+  root 'shelves#index'
+
   get 'shelves/index'
-
-  root 'stocks#index'
-
-  get 'stocks/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
