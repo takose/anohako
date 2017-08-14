568b6ed06450f747b1471d4e3eb7452dba1e79d7
tako <rina.m.takase@gmail.com>
2017-08-14 10:08:36 +0900

    Add image_url column

---
+++ app/javascript/drink.jsx
@@ -0,0 +1,26 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+export default class Drink extends React.Component {
+  static propTypes = {
+    product: PropTypes.object,
+    price: PropTypes.number
+  }
+  render () {
+    return (
+      <div className='drink'>
+        <div className='product-image-wrapper'>
+          <img className='product-image' src={this.props.product.image_url} width='100%' />
+        </div>
+        <div className='product-price'>
+          {this.props.price}
+        </div>
+      </div>
+    )
+  }
+}
---
+++ app/javascript/packs/lineup.js
@@ -0,0 +1,26 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Drink from "../drink"
+
+const Lineup = props => (
+  <div className='lineup'>
+    {props.drinks}
+  </div>
+)
+
+document.addEventListener('DOMContentLoaded', () => {
+    const node = document.getElementById('lineup')
+    const data = JSON.parse(node.getAttribute('data'))
+
+    const drinks = data.stocks.map((d) => <Drink key={d.id} product={d.product} price={d.price} />)
+    ReactDOM.render(<Lineup key={data.id} drinks={drinks} />, node)
+})
