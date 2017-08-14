f5aa165c78171dafd965926432f33b37762f19f1
tako <rina.m.takase@gmail.com>
2017-08-14 10:08:05 +0900

    Implement lineup component

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -1,5 +1,17 @@
 class ShelvesController < ApplicationController
   def index
-    @shelf = Shelf.first #FIXME
+    @shelf = Shelf.first.as_json(
+      only: [:id, :name],
+      include:
+      { stocks:
+        { only:
+          [:id, :price, :rest],
+          include:
+          { product:
+            { only: [:name, :image_url] }
+          }
+        }
+      }
+    )
   end
 end
