630eb0ac3a37aa786e1790d80ae2ab0843a721b1
tako <rina.m.takase@gmail.com>
2017-08-22 08:01:29 +0900

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Shelf.destroy_all
 Shelf.create(name: 'keita-lab')
 
+Product.destroy_all
 Product.create([
-  {name: 'redbull', image_url: 'https://i.gyazo.com/13c535a27502bab7647173eb39cea9a0.png'},
-  {name: 'monster', image_url: 'https://i.gyazo.com/cc4b757224ee2057247b2a9fbdfd9650.png'}
+  {name: 'lifeguard', image_url: 'https://i.gyazo.com/28aebdeb5abd38a4993c42f391d58a3d.png'},
+  {name: '三ツ矢サイダー', image_url: 'https://i.gyazo.com/fa234b9d6294150150066d62dff3af59.png'},
+  {name: 'cainz grape 100%', image_url: 'https://i.gyazo.com/9bb72be24cd6c119fda18c6aa4bba807.png'},
+  {name: 'すっきりオレンジ', image_url: 'https://i.gyazo.com/7d293ed3ab38984bf115bd87be1010c8.png'},
+  {name: ' CCレモン', image_url: 'https://i.gyazo.com/8923a942c00c5f582ff517fe4c853e6d.png'},
+  {name: 'オランジーナ', image_url: 'https://i.gyazo.com/6fb891d6115a566da9ebf613d1a8b2f9.png'}
 ])
 
-Stock.create([
-  {shelf_id: Shelf.first.id, product_id: Product.first.id, price: 200},
-  {shelf_id: Shelf.first.id, product_id: Product.second.id, price: 180}
-])
+Stock.destroy_all
+prices = [160, 140, 110, 100, 120, 140]
+Product.all.each_with_index do |p, idx|
+  Stock.create(shelf_id: Shelf.first.id, product_id: p.id, price: prices[idx])
+end
