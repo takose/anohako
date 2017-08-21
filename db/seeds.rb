402f7f9d7fc497ab1f4926a830e967e8782c0762
tako <rina.m.takase@gmail.com>
2017-08-22 01:08:21 +0900

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Shelf.create(name: 'keita-lab')
+
+Product.create([
+  {name: 'redbull', image_url: 'https://i.gyazo.com/13c535a27502bab7647173eb39cea9a0.png'},
+  {name: 'monster', image_url: 'https://i.gyazo.com/cc4b757224ee2057247b2a9fbdfd9650.png'}
+])
+
+Stock.create([
+  {shelf_id: Shelf.first.id, product_id: Product.first.id, price: 200},
+  {shelf_id: Shelf.first.id, product_id: Product.second.id, price: 180}
+])
