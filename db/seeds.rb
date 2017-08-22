a70ca162a9ace2fb9ddb88e012f78429c6ac7044
tako <rina.m.takase@gmail.com>
2017-08-22 10:13:20 +0900

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ Shelf.create(name: 'keita-lab')
 
 Product.destroy_all
 Product.create([
-  {name: 'lifeguard', image_url: 'https://i.gyazo.com/28aebdeb5abd38a4993c42f391d58a3d.png'},
-  {name: '三ツ矢サイダー', image_url: 'https://i.gyazo.com/fa234b9d6294150150066d62dff3af59.png'},
+  {name: ' CCレモン', image_url: 'https://i.gyazo.com/8923a942c00c5f582ff517fe4c853e6d.png'},
   {name: 'cainz grape 100%', image_url: 'https://i.gyazo.com/9bb72be24cd6c119fda18c6aa4bba807.png'},
   {name: 'すっきりオレンジ', image_url: 'https://i.gyazo.com/7d293ed3ab38984bf115bd87be1010c8.png'},
-  {name: ' CCレモン', image_url: 'https://i.gyazo.com/8923a942c00c5f582ff517fe4c853e6d.png'},
+  {name: 'lifeguard', image_url: 'https://i.gyazo.com/28aebdeb5abd38a4993c42f391d58a3d.png'},
+  {name: '三ツ矢サイダー', image_url: 'https://i.gyazo.com/fa234b9d6294150150066d62dff3af59.png'},
   {name: 'オランジーナ', image_url: 'https://i.gyazo.com/6fb891d6115a566da9ebf613d1a8b2f9.png'}
 ])
 
