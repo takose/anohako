58fa1e46e93dd13bb66bdf6b93614740ec4931a2
tako <rina.m.takase@gmail.com>
2017-09-27 23:23:29 +0900

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,6 @@ Product.create([
   {name: 'cainz grape 100%', image_url: 'https://i.gyazo.com/9bb72be24cd6c119fda18c6aa4bba807.png'},
   {name: 'すっきりオレンジ', image_url: 'https://i.gyazo.com/7d293ed3ab38984bf115bd87be1010c8.png'},
   {name: 'lifeguard', image_url: 'https://i.gyazo.com/28aebdeb5abd38a4993c42f391d58a3d.png'},
-  {name: '三ツ矢サイダー', image_url: 'https://i.gyazo.com/fa234b9d6294150150066d62dff3af59.png'},
-  {name: 'オランジーナ', image_url: 'https://i.gyazo.com/6fb891d6115a566da9ebf613d1a8b2f9.png'}
 ])
 
 Stock.destroy_all
