e9339d4618fc5586801a68c06974b926f3f01f95
tako <rina.m.takase@gmail.com>
2017-10-03 14:33:14 +0900

    Implement api Overview

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # == Route Map
 #
-#       Prefix Verb URI Pattern             Controller#Action
-#         root GET  /                       stocks#index
-# stocks_index GET  /stocks/index(.:format) stocks#index
+#         Prefix Verb URI Pattern                     Controller#Action
+#           root GET  /                               shelves#index
+#  shelves_index GET  /shelves/index(.:format)        shelves#index
+#    shelves_buy POST /shelves/buy(.:format)          shelves#buy
+# edit_api_shelf GET  /api/shelves/:id/edit(.:format) api/shelves#edit
 # 
 
 Rails.application.routes.draw do
@@ -10,6 +12,10 @@ Rails.application.routes.draw do
 
   get 'shelves/index'
   post 'shelves/buy'
+
+  namespace :api do
+    resources :shelves, only: [:update]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
