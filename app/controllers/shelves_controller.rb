56d46ade33094da5fc8c04294a0ac09fa0154554
tako <rina.m.takase@gmail.com>
2017-10-04 16:47:48 +0900

    Use environment value

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -20,11 +20,11 @@ class ShelvesController < ApplicationController
   def buy
     position = params[:position].to_i
     if position <= 1
-      @connect = Faraday.new(url: 'http://webmo-default.local')
+      @connect = Faraday.new(url: "http://#{ENV['FIRST_WEBMO_NAME']}.local")
     elsif position <= 3
-      @connect = Faraday.new(url: 'http://webmo02.local')
+      @connect = Faraday.new(url: "http://#{ENV['SECOND_WEBMO_NAME']}.local")
     elsif position <= 5
-      @connect = Faraday.new(url: 'http://webmo-screw.local')
+      @connect = Faraday.new(url: "http://#{ENV['THIRD_WEBMO_NAME']}.local")
     end
     if position % 2 == 0
       @connect.post('/api/rotate', { speed: "90", degree: "-60", absolute: "false" })
