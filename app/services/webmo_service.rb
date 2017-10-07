bfc69a9b24d21272982dbb8a976f2897a27f7a4c
tako <rina.m.takase@gmail.com>
2017-10-07 16:38:27 +0900

    Extract webmo function to webmo service

--- app/services/webmo_service.rb
+++ app/services/webmo_service.rb
@@ -1,2 +1,20 @@
 class WebmoService
+  class << self
+    def supply(position)
+      if position <= 1
+        @connect = Faraday.new(url: "http://#{ENV['FIRST_WEBMO_NAME']}.local")
+      elsif position <= 3
+        @connect = Faraday.new(url: "http://#{ENV['SECOND_WEBMO_NAME']}.local")
+      elsif position <= 5
+        @connect = Faraday.new(url: "http://#{ENV['THIRD_WEBMO_NAME']}.local")
+      end
+      if position % 2 == 0
+        @connect.post('/api/rotate', { speed: "90", degree: "-60", absolute: "false" })
+        @connect.post('/api/rotate', { speed: "90", degree: "60", absolute: "false" })
+      else
+        @connect.post('/api/rotate', { speed: "90", degree: "60", absolute: "false" })
+        @connect.post('/api/rotate', { speed: "90", degree: "-60", absolute: "false" })
+      end
+    end
+  end
 end
