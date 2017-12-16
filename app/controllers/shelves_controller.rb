55f9ed715cb5d6c2c9be5620e38484cc734ec939
tako <rina.m.takase@gmail.com>
2017-12-16 11:42:40 +0900

    for 1216

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -20,20 +20,16 @@ class ShelvesController < ApplicationController
   def buy
     position = params[:position].to_i
     webmo_name = [
-      '',
-      '',
-      '',
-      '',
-      '',
+      'webmoR',
+      'webmo01',
+      'webmo02',
+      'webmoL',
+      'webmo-default',
       ''
     ]
     @connect = Faraday.new(url: "http://#{webmo_name[position / 2]}.local")
     if position % 2 == 0
-      @connect.post('/api/rotate', { speed: "90", degree: "-60", absolute: "false" })
-      @connect.post('/api/rotate', { speed: "90", degree: "60", absolute: "false" })
-    else
-      @connect.post('/api/rotate', { speed: "90", degree: "60", absolute: "false" })
-      @connect.post('/api/rotate', { speed: "90", degree: "-60", absolute: "false" })
+      @connect.post('/api/rotate', { speed: "90", degree: "30", absolute: "false" })
     end
     redirect_to root_path
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,12 @@ Shelf.create(name: 'keita-lab')
 
 Product.destroy_all
 Product.create([
-  {name: ' CCレモン', image_url: 'https://i.gyazo.com/8923a942c00c5f582ff517fe4c853e6d.png'},
-  {name: 'cainz grape 100%', image_url: 'https://i.gyazo.com/9bb72be24cd6c119fda18c6aa4bba807.png'},
-  {name: 'すっきりオレンジ', image_url: 'https://i.gyazo.com/7d293ed3ab38984bf115bd87be1010c8.png'},
-  {name: 'lifeguard', image_url: 'https://i.gyazo.com/28aebdeb5abd38a4993c42f391d58a3d.png'},
+  {name: 'wonda', image_url: '/images/wonda.png'},
+  {name: 'cocacola', image_url: '/images/cola.png'},
+  {name: 'georgia black', image_url: '/images/black.png'},
+  {name: '十六茶', image_url: '/images/sixteen.png'},
+  {name: '三ツ矢サイダー', image_url: '/images/sider.png'},
+  {name: 'おーいお茶', image_url: '/images/tea.png'},
 ])
 
 Stock.destroy_all
